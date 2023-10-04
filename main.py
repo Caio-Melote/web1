215d30d58d925d98f5b10c0eae7378102ccd8dea
Melote <tcx3@outlook.com>
2023-10-03 21:22:22 -0300

    Comentários

--- main.py
+++ main.py
@@ -3,10 +3,22 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import psycopg2
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/chaves")
+# Configuração do CORS
+#O CORS (Cross-Origin Resource Sharing) é um mecanismo que usa cabeçalhos HTTP adicionais para informar a um navegador que permita que um aplicativo Web 
+# seja executado em uma origem (domínio) com permissão para acessar recursos selecionados de um servidor em uma origem distinta.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite requisições de todas as origens
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos HTTP
+    allow_headers=["*"],  # Permite todos os cabeçalhos HTTP
+)
+
+@app.get("/chaves_disp")
 def listar_chaves_disponiveis():
     try:
         # Conecta ao banco de dados PostgreSQL
@@ -34,4 +46,5 @@ def listar_chaves_disponiveis():
         return JSONResponse(content={"chaves": chaves})
     
     except (Exception, psycopg2.Error) as error:
+        # Em caso de erro, retorna o erro como resposta
         return JSONResponse(content={"error": str(error)})
